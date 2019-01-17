6bfe54f51ab245ad570c0b575abe7d506de6729d
toby <toby@vipcase.net>
2019-01-17 11:12:50 +0800

    errorlog

--- controller/serback/errorLog.php
+++ controller/serback/errorLog.php
@@ -1,9 +1,18 @@
 <?php
 ini_set("memory_limit",-1);
 
-if($_POST) $console->alert("error",-1);
-
 $module["aceEditor"]["name"] = '_aceEditor';//POST欄位名稱，不可使用"aceEditor"
+
+if($_POST){
+	$allowUser = array("vipadmin");
+	if(!in_array($member->getInfo("account"),$allowUser)){
+		$console->alert($console->getMessage("NOT_AUTHORITY"),-1);
+		exit;
+	}
+	file_put_contents(ini_get("error_log"),$_POST[$module["aceEditor"]["name"]]);
+	$console->alert("OK",$_SERVER["REQUEST_URI"]);
+}
+
 // file_put_contents(ini_get("error_log"),"");
 $data[$module["aceEditor"]["name"]] = "";
 if(is_file(ini_get("error_log"))){
